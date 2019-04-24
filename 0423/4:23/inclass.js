20ba69a40a20fefd8d7992bbc06b9f1f2bf0ab63
siyuhang23 <42788372+siyuhang23@users.noreply.github.com>
2019-04-23 20:27:45 -0400

    Add files via upload

---
+++ 0423/4:23/inclass.js
@@ -0,0 +1,25 @@
+//check if document is ready
+$(document).ready(function(){
+
+//get our dic size
+var doc = $(document);
+
+var width = $(document).width()-100;
+var height = $(document).height()-100;
+
+// console.log($(".circle"));
+//target our shapes
+var shapes=[$(".one"),$(".two"),$(".three")]
+
+//create a random shape and position
+setInterval(function randomStuff(){
+var randomShape = shapes[Math.floor(Math.random()*shapes.length)]
+var clone = randomShape.clone().appendTo('body');
+// console.log(clone)
+
+clone.last().css('left',Math.random()*width)
+clone.last().css('top',Math.random()*height)
+},200)
+
+
+})
